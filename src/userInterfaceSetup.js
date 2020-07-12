ffbb2049625027d7d90e081c54132c705f22e1ac
MatteoWebDeveloper <matteowebdesigner@googlemail.com>
2020-07-12 12:38:27 +0100

    do not start if it's undefined and null

--- src/userInterfaceSetup.js
+++ src/userInterfaceSetup.js
@@ -4,7 +4,7 @@ const { saveUserSetup } = require('./setupInstance');
 const startInterfaceSetup = async () => {
     await browser.page.goto('http://localhost:3000/');
     
-    await browser.page.waitFor(eval(`() => window.puppeteerData !== null`), { timeout: 0 });
+    await browser.page.waitFor(eval(`() => window.puppeteerData !== undefined && window.puppeteerData !== null`), { timeout: 0 });
 
     const formData = await browser.page.evaluate(eval(`async () => window.puppeteerData`));
 
